ad3dc0ee3f265c06b6384ec6cbe4342594bc0ecb
Filip Komogorov <filipkomogorov@gmail.com>
2023-03-27 21:44:00 +0200

    adding the banner frame

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,8 +2,18 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      spacing: {
+        sizeSmall: '8px',
+        sizeMedium: '16px',
+        sizeLarge: '24px',
+        sizeXl: '32px'
+      },
       width: {
-        'screenWidthLarge': '128rem'
+        screenWidthLarge: '128rem',
+        screenWidthMedium: '110rem'
+      },
+      height: {
+        bannerHeightDesktop: '40rem'
       },
       colors: {
         white: '#fafafa',
